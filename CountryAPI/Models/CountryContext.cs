b38cf9298f3e6b56f35021f48fa7a366fe47eb14
MenushiLakshikaBISTE <MenushiLakshikaBISTE@BISTECNB107>
2022-02-22 14:48:40 +0530

    Add project files.

---
+++ CountryAPI/Models/CountryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CountryAPI.Models
+{
+    public class CountryContext : DbContext
+    {
+        public CountryContext(DbContextOptions<CountryContext> options)
+           : base(options)
+        {
+           
+        }
+        public DbSet<CountryItem>? CountryItems { get; set; } = null;
+
+    }
+}
---
+++ CountryAPI/Models/CountryItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CountryAPI.Models
+{
+    public class CountryItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
